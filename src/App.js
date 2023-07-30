2065455423793bb352bc7db1e70eff1282765698
AhmetErenLacinbala <ahmetlacinbala@gmail.com>
2023-07-30 03:00:54 +0300

    rocket is added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OrbitControls, Sphere, SpotLight, meshBounds, useHelper } from '@react-three/drei'
+import { OrbitControls, PerformanceMonitor, Sphere, SpotLight, meshBounds, useHelper } from '@react-three/drei'
 import { DoubleSide } from 'three';
 import { Suspense, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import * as THREE from 'three'
@@ -10,6 +10,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { AmbientLight } from 'three';
 import { Bloom, EffectComposer, SSAO } from '@react-three/postprocessing'
 import { BlurPass, Resizer, KernelSize, Resolution } from 'postprocessing'
+import { Perf } from 'r3f-perf';
 
 const camprops = [
   {
@@ -56,7 +57,7 @@ function Line({ start, end, bulletPos, meshPos }) {
   useFrame(() => {
 
 
-    if (bulletPos[0] < -2 && bulletPos[0] > -2.05 && meshPos[0] === -2) {
+    if (bulletPos[0] < -2 && bulletPos[0] > -2.1 && meshPos[0] === -2) {
 
       ref.current.geometry.setFromPoints([start, bulletPos].map((point) => new THREE.Vector3(...point)));
     }
@@ -73,7 +74,8 @@ function Line({ start, end, bulletPos, meshPos }) {
 
 function Cam(props) {
   //https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/camera.glb
-  const { scene } = useLoader(GLTFLoader, '/models/camera.glb');
+  // /models/camera.glb
+  const { scene } = useLoader(GLTFLoader, 'https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/camera.glb');
   const { meshProps, lineEnd, rotationY, rotationZ, alignment, bulletPos } = props;
 
 
@@ -92,50 +94,114 @@ function Cam(props) {
 }
 
 function Bullet(props) {
+  //
+  // /models/rpg.glb
+  const rpg = useLoader(GLTFLoader, '/models/rpg.glb');
   const { meshProps, setBulletPos } = props;
   const [flag, setflag] = useState(1);
   const [random, setRandom] = useState(Math.random())
-  const ref = useRef();
+  const bulletRef = useRef();
   const inRef = useRef();
   const outRef = useRef();
+  const particalRef = useRef();
+  useEffect(() => {
+    let particleGeometry = new THREE.BufferGeometry();
+    let particlePositions = new Float32Array(1000 * 3);
+  
+    // Set all positions to zero
+    for (let i = 0; i < 1000; i++) {
+      particlePositions[i * 3 + 0] = 0;
+      particlePositions[i * 3 + 1] = 0;
+      particlePositions[i * 3 + 2] = 0;
+    }
+  
+    particleGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+  
+    let particleMaterial = new THREE.PointsMaterial({
+      color: 0xffffff,
+      size: 0.05,
+    });
+  
+    particalRef.current.geometry = particleGeometry;
+    particalRef.current.material = particleMaterial;
+    console.log(particalRef.current)
+  }, []);
+  
   useFrame((state, delta) => {
-    ref.current.position.x += 2 * delta;
-    ref.current.position.y += 0.5 * flag * (random / 5) * delta;
-    if (ref.current.position.x > 4) {
-      ref.current.position.x = -4;
-      ref.current.position.y = 1;
+    bulletRef.current.position.x += 3 * delta;
+    bulletRef.current.position.y += 0.75 * flag * (random/5) * delta;
+
+    let positions = particalRef.current.geometry.attributes.position.array;
+
+    //console.log(positions);
+    for (let i = 0; i < 1000; i++) {
+
+      // Assign a random speed to the particle
+      var speed = Math.random() * 0.5;
+
+      // Move the particle in the -z direction at the random speed
+      positions[i * 3 + 1] -= speed;
+
+      //expanding on x and y axis
+      positions[i * 3 + 0] += (Math.random() - 0.5) * 0.08;
+      positions[i * 3 + 2] += (Math.random() - 0.5) * 0.08;
+
+      // Check if the particle has reached the reset point
+      if (positions[i * 3 + 1] < -2) {
+          // Reset the particle back to its starting position
+          positions[i * 3 + 0] = 0;
+          positions[i * 3 + 1] = 0;
+          positions[i * 3 + 2] = 0;
+      }
+  }
+
+  particalRef.current.geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  // Update the particle positions in the buffer geometry
+  //particalRef.current.geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    if (bulletRef.current.position.x > 4) {
+      bulletRef.current.position.x = -4;
+      bulletRef.current.position.y = 1;
       setRandom(Math.random())
       if (Math.random() < 0.5) setflag(-1)
       else setflag(1)
     }
     
-    setBulletPos([ref.current.position.x, ref.current.position.y, ref.current.position.z]);
-    if (Math.floor(ref.current.position.x*20)/20 === -2){
-      inRef.current.position.set(...ref.current.position);
+    setBulletPos([bulletRef.current.position.x, bulletRef.current.position.y, bulletRef.current.position.z]);
+    if (Math.floor(bulletRef.current.position.x*20)/20 === -2){
+      inRef.current.position.set(...bulletRef.current.position);
       inRef.current.position.x -= 0.03;
     }
-    else if (Math.floor(ref.current.position.x*20)/20 === 2) {
-      outRef.current.position.set(...ref.current.position);
+    else if (Math.floor(bulletRef.current.position.x*20)/20 === 2) {
+      outRef.current.position.set(...bulletRef.current.position);
     }
-    if(ref.current.position.x <-2){
+    if(bulletRef.current.position.x <-2){
       outRef.current.visible = false;
       inRef.current.visible = false;
     }
-    else if (Math.floor(ref.current.position.x*20)/20 === -2) {
+    else if (Math.floor(bulletRef.current.position.x*20)/20 === -2) {
       inRef.current.visible = true;
     }
 
-    else if (Math.floor(ref.current.position.x*20)/20 === 2) {
+    else if (Math.floor(bulletRef.current.position.x*20)/20 === 2) {
       outRef.current.visible = true;
     }
   });
+/* 
+      <sphereGeometry args={[0.05, 16, 16]} />
+      <meshStandardMaterial metalness={0.9} color={'gray'} />
 
+      ref.current.rotation.z = (Math.atan((flag*(0.75*(random))/(3)))/Math.PI - Math.PI/2);
+*/
   return (
     <mesh>
 
-    <mesh ref={ref} {...meshProps}>
-      <sphereGeometry args={[0.05, 16, 16]} />
-      <meshStandardMaterial metalness={0.9} color={'gray'} />
+    <mesh scale={0.33}  ref={bulletRef} {...meshProps}>
+      <mesh rotation={[0,0, Math.atan((flag*(0.75*(random/5))/(3)))/Math.PI - Math.PI/2]}>
+      <primitive object={rpg.scene}/>
+      <mesh ref={particalRef}>
+
+      </mesh>
+      </mesh>
     </mesh>
     <mesh ref={inRef}>
       <sphereGeometry args={[0.05, 16, 16]} />
@@ -179,8 +245,9 @@ function BulletTrack(props) {
 }
 function Stick(props) {
   //https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/stick.glb
+  ///models/stick.glb
   const { args } = props;
-  const stick_model = useLoader(GLTFLoader, '/models/stick.glb');
+  const stick_model = useLoader(GLTFLoader, 'https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/stick.glb');
   return (
     <mesh scale={[0.5, 1.7, 0.5]} {...args}>
 
@@ -190,9 +257,10 @@ function Stick(props) {
 }
 
 function LightStick(props) {
-  //https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/stick.glb
+  //https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/light.glb
+  //models/light.glb
   const { args } = props;
-  const stick_model = useLoader(GLTFLoader, '/models/light.glb');
+  const stick_model = useLoader(GLTFLoader, 'https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/light.glb');
   return (
     <mesh rotation={[-Math.PI/2, Math.PI ,0]} {...args}>
 
@@ -206,9 +274,10 @@ function LightStick(props) {
 
 
 function Plane(props) {
-  //https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/stick.glb
+  //https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/plane.glb
+  ///models/plane.glb
   const { args } = props;
-  const stick_model = useLoader(GLTFLoader, '/models/plane.glb');
+  const stick_model = useLoader(GLTFLoader, 'https://raw.githubusercontent.com/AhmetErenLacinbala/avci_3d/master/public/models/plane.glb');
   return (
     <mesh scale={[0.5, 1.7, 0.5]} {...args}>
 
@@ -233,8 +302,6 @@ function BulletPoint(props){
   },[]);
 
   useFrame(()=>{
-    if(location[0]>0)
-    console.log(ref.current);
     ref.current.position.set(new THREE.Vector3(0,0,0));
   })
 
@@ -345,7 +412,8 @@ function App() {
 
   return (
     <div className="App">
-      <Canvas frameloop="always" camera={{ position: [0, 0, 8] }}>
+      <Canvas frameloop="demand" camera={{ position: [0, 0, 8] }}>
+      <Perf/>
       <Plane/>
     <ambientLight args={["0x404040", "4"]}/>
         <mesh>
